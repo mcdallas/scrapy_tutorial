3ac4d15297cf3568a2cabe52978f68c4e018dd58
Mike Dallas <mc-dallas@hotmail.com>
2017-02-19 20:23:24 +0000

    add items

--- tutorial/items.py
+++ tutorial/items.py
@@ -5,10 +5,16 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class TutorialItem(scrapy.Item):
+class TutorialItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class Quote(Item):
+    text = Field()
+    author = Field()
+    tags = Field()
--- tutorial/spiders/quotes.py
+++ tutorial/spiders/quotes.py
@@ -1,4 +1,5 @@
 import scrapy
+from tutorial.items import Quote
 
 
 class QuotesSpider(scrapy.Spider):
@@ -13,12 +14,11 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').extract_first(),
-                'author': quote.css('small.author::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
-            }
-
+            item = Quote()
+            item['text'] = quote.css('span.text::text').extract_first()
+            item['author'] = quote.css('small.author::text').extract_first()
+            item['tags'] = quote.css('div.tags a.tag::text').extract()
+            yield item
         next_page = response.css('li.next a::attr(href)').extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
